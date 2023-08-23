ce6b0f1e0c13cebfd37127bb93c051bbf28825f1
Aleksey <al-bat-ros@mai.ru>
2023-08-24 02:18:07 +0500

    hw02_unpack_string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,40 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	nStr := []rune(str)
+	var resultStr strings.Builder
+	var count int
+
+	for i := 0; i < len(nStr); i++ {
+		if unicode.IsDigit(nStr[0]) {
+			return "", ErrInvalidString
+		}
+
+		if i < len(nStr)-1 && unicode.IsDigit(nStr[i]) && unicode.IsDigit(nStr[i+1]) {
+			return "", ErrInvalidString
+		}
+
+		if i < len(nStr)-1 && unicode.IsDigit(nStr[i+1]) {
+			num, _ := strconv.Atoi(string(nStr[i+1]))
+			count = num
+			if string(nStr[i]) != "\n" {
+				mulStr := strings.Repeat(string(nStr[i]), count)
+				resultStr.WriteString(mulStr)
+			} else {
+				mulStr := strings.Repeat("\\n", count)
+				resultStr.WriteString(mulStr)
+			}
+		} else if !unicode.IsDigit(nStr[i]) && string(nStr[i]) != "\n" {
+			resultStr.WriteString(string(nStr[i]))
+		}
+	}
+	return resultStr.String(), nil
 }
